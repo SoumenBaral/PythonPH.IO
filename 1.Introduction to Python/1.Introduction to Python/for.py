42ba80574f68044c5325c2db217184a591e53eb9
SoumenBaral <shumen.sb@gmail.com>
2023-09-24 19:26:51 +0600

    day 1 is done with python

---
+++ 1.Introduction to Python/1.Introduction to Python/for.py
@@ -0,0 +1,26 @@
+# numbers = [10,20,30,40,50]
+# sum = 0
+# for num in numbers :
+#     # print(num)
+#     sum +=num
+#     if(sum>100):
+#         print('Bigger value ')
+
+# print(sum)
+
+# text = 'pagla Hower tore'
+# for char in text:
+#     print(char)
+
+# for i in range(1,10 ,2 ):
+#     print(i)
+
+
+# friends =['shakib','shuvo','rakib','durjoy']
+# for index, friend in enumerate(friends) :
+#     print(f"Friend No {index}---{friend}")
+
+# # if we want to get index then we have to use enumerate
+
+for i in range(5):
+    print(i)
