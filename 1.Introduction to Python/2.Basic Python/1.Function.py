f913dbf55c1f8fb6f6ebdeabd578f435072a4f37
SoumenBaral <shumen.sb@gmail.com>
2023-09-24 21:29:37 +0600

    function

---
+++ 1.Introduction to Python/2.Basic Python/1.Function.py
@@ -0,0 +1,16 @@
+# # define the function thats why we use def 
+def fun (x,y):
+    return x+y
+# print(fun(2,3))
+
+
+def doubleIt(num):
+    print(num*2)
+    return num*2
+
+# doubleIt(7)
+# doubleIt(22)
+total = fun(22,10)
+print(total)
+final = doubleIt(total)
+print('Final Result is : ', final)
