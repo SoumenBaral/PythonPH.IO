db699ed4f32a073aef7b8e957ab9e1b12acf7dfa
SoumenBaral <shumen.sb@gmail.com>
2023-09-24 13:40:13 +0600

    variable is done

--- 1.Introduction to Python/1.Introduction to Python/variable.py
+++ 1.Introduction to Python/1.Introduction to Python/variable.py
@@ -6,5 +6,7 @@ is_single = False
 print(type(district))
 print(age+interest_rate)
 print(type(is_single))
-# multiline Comment is alt shift A
+# multiline Comment is alt shift A is called docstring 
 """ single line comment is ctrl /  """
+text = f"kodom ali ia {age} and his life saver is {name} . lives in {district}"
+print(text)
