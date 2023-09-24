a84751a7d51c459f97fd6017839ef4565a7255c3
SoumenBaral <shumen.sb@gmail.com>
2023-09-24 17:42:20 +0600

    input understand;

---
+++ 1.Introduction to Python/1.Introduction to Python/input.py
@@ -0,0 +1,13 @@
+# print('i am good man')
+# # input()
+# money = input('Give me some money : ')
+# print(money)
+firstMoney = input('kodom ali , dost kisu taka day : ')
+secondMoney = input('jorina, dost kiso taka day : ')
+print(f'kodom ai :  {firstMoney} jorina : {secondMoney}')
+total1 = firstMoney+secondMoney
+print("Total Money : ",type(total1))
+
+# By default the input are string type
+total = int(firstMoney)+int(secondMoney)
+print(total)
