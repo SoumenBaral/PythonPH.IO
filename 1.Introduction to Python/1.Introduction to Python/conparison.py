0f3daab834ec3b3b59c884714939b566a66367f7
SoumenBaral <shumen.sb@gmail.com>
2023-09-24 18:13:49 +0600

    operator and comparision

---
+++ 1.Introduction to Python/1.Introduction to Python/conparison.py
@@ -0,0 +1,6 @@
+# > < , >=, <=  ,== ,!= 
+# python don't have the ++,-- operator 
+ 
+num1 =12 
+num2 =54
+if(num1>num2): print('number 1 is big')
---
+++ 1.Introduction to Python/1.Introduction to Python/oparetor.py
@@ -0,0 +1,10 @@
+# + - * / % //(flor modulus ) ----maximum time the operator are same for any programming language .
+first = 32 
+second = 3
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+print(first % second)
+print(first // second)
+
