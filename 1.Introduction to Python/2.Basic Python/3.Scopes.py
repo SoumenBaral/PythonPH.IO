35d211cd42012ee46d4c51ddfeb7fa9e70ee02f2
SoumenBaral <shumen.sb@gmail.com>
2023-09-25 20:27:10 +0600

    scope

--- 1.Introduction to Python/2.Basic Python/3.Scopes.py
+++ 1.Introduction to Python/2.Basic Python/3.Scopes.py
@@ -1,7 +1,7 @@
 balance = 300 # Global variable we can use it any where 
 def fun():
     name = 'Soumen' #Its a local variable it can only use into the function and condition
-    global balance
+    global balance #if we want to modify the global value then we have to write the keyword global 
     balance = balance - 100
     print(balance)
 
