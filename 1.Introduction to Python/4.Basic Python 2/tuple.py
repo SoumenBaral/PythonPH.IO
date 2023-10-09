e059319c4bf63b73368ef2f1d80a189d5f8af632
SoumenBaral <shumen.sb@gmail.com>
2023-10-09 14:42:36 +0600

    tuple is done

--- 1.Introduction to Python/4.Basic Python 2/tuple.py
+++ 1.Introduction to Python/4.Basic Python 2/tuple.py
@@ -5,3 +5,27 @@ def multiple():
 
 things = 'pen','fan','laptop','table' #accidental tuple when we forget to withdraw coma;
 print(things) 
+print(type(things))
+print(things[0])
+print(things[-3])#we can get the value in back by the using of minus  and it starts with -1
+print(things[::-1])#we can reverser any string via it 
+print(things[1:4]) #we can slice it 
+print('\n')
+
+if 'pen'and 'fan' in things:
+    print(
+        "Exist"
+    )
+
+for item in things:
+    print(item)
+
+# things[0]='wagon' it is immutable we can not change it or assign it after declaration 
+
+num = ([2,3,4],[34,33,12,35],[34,212,443,2212])
+# print(type(num))
+print(num[0])
+# num[0]=[3,45] we Can not change it but
+
+num[0][2] =33
+print(num)
