a920d44c2fbb5a6799642f5aa0feeebc30fe0a6f
SoumenBaral <shumen.sb@gmail.com>
2023-10-11 04:30:32 +0600

    Exception

---
+++ 1.Introduction to Python/4.Basic Python 2/7.Exception.py
@@ -0,0 +1,8 @@
+try:
+    # result = 23/0 # Wrong Approach 
+    x = 12/2 # it is right Approach it don't enter the except Call
+except:
+    print('Error Happened')
+finally:
+    print('it wee be always with it ')
+print('done')
