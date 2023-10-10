ec92fb1262e1dc89be1dd8d17ccc0d500e5a73ed
SoumenBaral <shumen.sb@gmail.com>
2023-10-11 04:25:52 +0600

    some python pakege

---
+++ 1.Introduction to Python/4.Basic Python 2/5.Built-in_Modules.py
@@ -0,0 +1,14 @@
+from math import *
+from random import* 
+from time import sleep
+list = [1,2,3,4,5,6]
+
+upperNumber =ceil(4.564)
+print(sqrt(16))
+print(factorial(6))
+print(upperNumber) 
+
+print(random())
+print(randint(1,100))
+sleep(3)
+print(choice(list))
