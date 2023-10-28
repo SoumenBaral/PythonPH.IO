f1aa4412fbe8d0d3f521a08913bdf5857e5a76db
SoumenBaral <shumen.sb@gmail.com>
2023-10-28 15:39:49 +0600

    operator

--- 1.Introduction to Python/1.Introduction to Python/recapDay1.py
+++ 1.Introduction to Python/1.Introduction to Python/recapDay1.py
@@ -2,23 +2,31 @@ print('Recap Day Start....')
 # --------variable---------
 age = 23
 tax = 33.32
-print('total price is = ',age*tax)
+# print('total price is = ',age*tax)
 isActive = True
 name = ' Soumen'
 Address = "Pasharibunia Bhandria pirojpur"
 text = f'My name is {name} and my address is {Address}'# very use full we use it in js via caret sign ;
-print(text)
+# print(text)
 
 # ================Taking input==========
 print("I am a honest man")
-money = input('Give me some money : ') 
-print(int(money)+100)
-firstMoney = input('Shakib khan money : ')
-secondMoney = input('Porimoni money : ')
-print(f'shakib khane gave {firstMoney} and poriMoni gave {secondMoney}')
-total = firstMoney + secondMoney
-print('false total is == ',total)
-print(type(total))
-actualTotal = int(firstMoney) +int(secondMoney)
-print('Actual total is ', actualTotal)
-print(type(actualTotal))
+# money = input('Give me some money : ') 
+# print(int(money)+100)
+# firstMoney = input('Shakib khan money : ')
+# secondMoney = input('Porimoni money : ')
+# print(f'shakib khane gave {firstMoney} and poriMoni gave {secondMoney}')
+# total = firstMoney + secondMoney
+# print('false total is == ',total)
+# print(type(total))
+# actualTotal = int(firstMoney) +int(secondMoney)
+# print('Actual total is ', actualTotal)
+# print(type(actualTotal))
+
+# Operator 
+
+# + , - , * , / , % , //(flor modulus) 
+first = 33
+second = 3
+print(first%second) # vagses
+print(first//second) # vagfol 
