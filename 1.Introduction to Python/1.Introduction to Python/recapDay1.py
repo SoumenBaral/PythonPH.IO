c96ecad25903e860b15e08b9c1ba5a01718364ab
SoumenBaral <shumen.sb@gmail.com>
2023-10-28 15:19:35 +0600

    Start racap

---
+++ 1.Introduction to Python/1.Introduction to Python/recapDay1.py
@@ -0,0 +1,10 @@
+print('Recap Day Start....')
+age = 23
+tax = 33.32
+print('total price is = ',age*tax)
+isActive = True
+name = ' Soumen'
+Address = "Pasharibunia Bhandria pirojpur"
+text = f'My name is {name} and my address is {Address}'# very use full we use it in js via caret sign ;
+print(text)
+ 
