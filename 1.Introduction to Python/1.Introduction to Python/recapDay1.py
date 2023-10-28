5c64226b6189f1dc749f36401f2ad9fae2b06594
SoumenBaral <shumen.sb@gmail.com>
2023-10-28 15:33:05 +0600

    input taking

--- 1.Introduction to Python/1.Introduction to Python/recapDay1.py
+++ 1.Introduction to Python/1.Introduction to Python/recapDay1.py
@@ -1,4 +1,5 @@
 print('Recap Day Start....')
+# --------variable---------
 age = 23
 tax = 33.32
 print('total price is = ',age*tax)
@@ -7,4 +8,17 @@ name = ' Soumen'
 Address = "Pasharibunia Bhandria pirojpur"
 text = f'My name is {name} and my address is {Address}'# very use full we use it in js via caret sign ;
 print(text)
- 
+
+# ================Taking input==========
+print("I am a honest man")
+money = input('Give me some money : ') 
+print(int(money)+100)
+firstMoney = input('Shakib khan money : ')
+secondMoney = input('Porimoni money : ')
+print(f'shakib khane gave {firstMoney} and poriMoni gave {secondMoney}')
+total = firstMoney + secondMoney
+print('false total is == ',total)
+print(type(total))
+actualTotal = int(firstMoney) +int(secondMoney)
+print('Actual total is ', actualTotal)
+print(type(actualTotal))
