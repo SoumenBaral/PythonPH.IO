6a3acd213f6b36e67fb646043e23776a981b8dee
SoumenBaral <shumen.sb@gmail.com>
2023-10-30 22:59:44 +0600

    day2;

---
+++ 1.Introduction to Python/2.Basic Python/recapDay2.py
@@ -0,0 +1,17 @@
+# we use def for function thats means define the function 
+# Start day 2
+def add (x,y):
+    addition = x+y
+    return addition
+
+addition = add(10,20)
+# print(addition)
+
+def doubleIt(x):
+    return x*2
+
+double = doubleIt(20)
+print(double)
+
+
+
