f7bd201d33d88c71919faec91b9a0a23cf1ca800
SoumenBaral <shumen.sb@gmail.com>
2023-11-04 11:18:08 +0600

    type of variable

---
+++ 1.Introduction to Python/3.Practice/ConceptualSession.py
@@ -0,0 +1,14 @@
+# type 
+x = 3.3
+print(type(x))
+y = 3 + 5j
+print(type(y))
+
+a = int(3.53)#male int 
+print(a)
+
+b = float(5)
+print(b)
+
+c = complex(5,2)#make it complex number we need to take 2 number first real number and second imaginary number
+print(c)
