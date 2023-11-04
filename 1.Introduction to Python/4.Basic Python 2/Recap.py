9ebf1b02518d56f77278f2002ee4bbe8761b8f60
SoumenBaral <shumen.sb@gmail.com>
2023-11-04 23:42:56 +0600

    tuple

--- 1.Introduction to Python/4.Basic Python 2/Recap.py
+++ 1.Introduction to Python/4.Basic Python 2/Recap.py
@@ -26,3 +26,38 @@ else:
 
 print(name.upper())
 print(name2.title())
+
+# ===============================================================>
+
+# xxxxxxx...... Tuple.....xxxxxxx
+
+# we use 3rd bracket for list but incase of tuple we use 1st bracket
+# we can return multiple things form the function
+
+def multiple():
+    return 3,5
+print(multiple())
+things = 'pen','fan','laptop','table' #accidental tuple when we forget to withdraw coma;
+print(things) # Accidental tuple 
+print(type(things)) 
+print(things[-3])
+print(things[::-1])
+print(things[1:3])
+print()
+
+if 'fan' and 'pen' in things:
+    print("Exist")
+else:
+    print("Not found")
+
+# things[0] ='wagon' it's not possible immutable
+
+nums = ([2,3,4],[34,33,12,35],[34,212,443,2212],"Soumen","Baral",'shuvo')
+
+for pr in nums:
+    # print(pr) 
+    for val in pr:
+        print(val) #Single value print 
+
+# Final thing it that we use tuple for returning the multiple value from the function 
+
