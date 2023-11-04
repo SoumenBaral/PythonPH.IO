2d8c45084627249ab20e1f1116a33a7ffedd389f
SoumenBaral <shumen.sb@gmail.com>
2023-11-04 23:16:37 +0600

    recap day start

---
+++ 1.Introduction to Python/4.Basic Python 2/Recap.py
@@ -0,0 +1,19 @@
+#Explore String 
+
+name = 'Soumen\'s laptop' # we can escape the quit
+print(name)
+# MUltiline String 
+name = """ 
+    Salmon Khan 
+    Number nai brother in low.
+    that a cute slang
+"""
+print(name)
+
+# F sting that's look like carate in js
+name2 = f'I know the man he is {name}' 
+print (name2)
+name3 = 'Shakib'
+
+print(name3[3])
+# String is immutable thinks thats means we can't change it
