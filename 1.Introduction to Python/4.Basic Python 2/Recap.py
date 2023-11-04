5eb906f31363b73badb0c020f14b2d807200ac0e
SoumenBaral <shumen.sb@gmail.com>
2023-11-04 23:25:36 +0600

    string

--- 1.Introduction to Python/4.Basic Python 2/Recap.py
+++ 1.Introduction to Python/4.Basic Python 2/Recap.py
@@ -1,13 +1,11 @@
 #Explore String 
 
-name = 'Soumen\'s laptop' # we can escape the quit
-print(name)
+names = 'Soumen\'s laptop' # we can escape the quit
+print(names)
 # MUltiline String 
-name = """ 
-    Salmon Khan 
+name = """Salmon Khan 
     Number nai brother in low.
-    that a cute slang
-"""
+    that a cute slang"""
 print(name)
 
 # F sting that's look like carate in js
@@ -15,5 +13,16 @@ name2 = f'I know the man he is {name}'
 print (name2)
 name3 = 'Shakib'
 
-print(name3[3])
+print(name3[3]) # print just 3 number index
+print(name[2:11]) # print 2 to 11 number index
+print(name[-3]) # print last 3 number index start with -1
+print(name3[::-1]) #Print reverse
 # String is immutable thinks thats means we can't change it
+
+if "laptop" in names:
+    print("Exist")
+else:
+    print("No")
+
+print(name.upper())
+print(name2.title())
