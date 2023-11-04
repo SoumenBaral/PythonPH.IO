5b786f2b147c287d2d8aaad91984cf27740da3f4
SoumenBaral <shumen.sb@gmail.com>
2023-11-04 12:54:14 +0600

    find function

--- 1.Introduction to Python/3.Practice/ConceptualSession.py
+++ 1.Introduction to Python/3.Practice/ConceptualSession.py
@@ -12,3 +12,44 @@ print(b)
 
 c = complex(5,2)#make it complex number we need to take 2 number first real number and second imaginary number
 print(c)
+# we can not convert complex to int or float 
+#  we can take 1 parameter for making complex 
+d = complex(4) # output will be 4 + 0j;
+print(d)
+
+# String is immutable means we can't change the value after declaration 
+
+xx = "Hello"
+# xx[0] = 'G' it's not possible 
+
+#  Sting is Iterable 
+
+for char in xx:
+    print(char,end='')
+print()
+
+
+# Some builtIn function in python 
+# Capitalize || upper || lower || casefold || find || count || Replace 
+ 
+
+name = "soumen"
+
+print(name.capitalize()) # Soumen
+print(name.upper()) # SOUMEN  
+print(name.lower()) # soumen
+print(name.casefold()) # same as lower case 
+
+
+# find 
+
+soumen = "Hello world"
+src = "world"
+index = soumen.find(src)
+print(index)
+
+if index !=-1:
+    print(f"We just find '{src}' in the index of -- '{index}' ")
+
+else:
+    print("Not found")
