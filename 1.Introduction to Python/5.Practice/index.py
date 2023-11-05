ee69aed7e87e91b4bbbc0a16d64555888754b20b
SoumenBaral <shumen.sb@gmail.com>
2023-11-05 09:16:04 +0600

    alt of while loop

--- 1.Introduction to Python/5.Practice/index.py
+++ 1.Introduction to Python/5.Practice/index.py
@@ -5,7 +5,7 @@ print(id(b))
 
 # there address will be same python memory management system handle it
 
-a = int(input()) # they always take input as String 
+# a = int(input()) # they always take input as String 
 
 # one line multiple input 
 # multiple line multiple input
@@ -14,8 +14,26 @@ a = int(input()) # they always take input as String
 # 1
 lst = []
 
-for i in range(0,a):
-    x = int(input())
-    lst.append(x)
+# for i in range(0,a):
+#     x = int(input())
+#     lst.append(x)
 
+# print(lst)
+
+# Split || join 
+
+# split => string to list 
+# join => list to string 
+
+# lst = list(map(int,input().split()))
 print(lst)
+
+lst = [1,3,4,5,6]
+lt = list(map(lambda x: x*2 , lst))
+print(lt)
+
+testCase = int(input())
+
+range(val)-->start with 0 and end with val-1
+for i in range(testCase):
+    print(i)
