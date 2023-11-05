282bae0bc8a4d703c50fd27968538e60e9a115fd
SoumenBaral <shumen.sb@gmail.com>
2023-11-05 08:56:26 +0600

    take input in single line mulitiple input

--- 1.Introduction to Python/5.Practice/index.py
+++ 1.Introduction to Python/5.Practice/index.py
@@ -4,3 +4,18 @@ print(id(a))
 print(id(b))
 
 # there address will be same python memory management system handle it
+
+a = int(input()) # they always take input as String 
+
+# one line multiple input 
+# multiple line multiple input
+
+
+# 1
+lst = []
+
+for i in range(0,a):
+    x = int(input())
+    lst.append(x)
+
+print(lst)
