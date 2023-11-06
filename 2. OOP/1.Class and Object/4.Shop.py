268aa61066151e6240255175b30e4b82e9e7cf9a
SoumenBaral <shumen.sb@gmail.com>
2023-11-06 19:46:16 +0600

    shopping done

--- 2. OOP/1.Class and Object/4.Shop.py
+++ 2. OOP/1.Class and Object/4.Shop.py
@@ -5,3 +5,13 @@ class shop:
 
     def add_to_Cart(self,product):
         self.cart.append(product)
+
+
+soumen = shop("Soumen")
+soumen.add_to_Cart("Table")
+soumen.add_to_Cart("chair")
+Antora = shop("Antora")
+Antora.add_to_Cart("Wordwrap")
+Antora.add_to_Cart("Bike")
+
+print(Antora.cart)
