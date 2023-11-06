ffcdd6113c6bd6449c3b21712a7a280678ed2438
SoumenBaral <shumen.sb@gmail.com>
2023-11-06 21:30:34 +0600

    under construction school

--- 2. OOP/1.Class and Object/7.School.py
+++ 2. OOP/1.Class and Object/7.School.py
@@ -14,8 +14,28 @@ class Teacher:
         self.id = id
     def __repr__(self) -> str:
         return f'Name: {self.name} Subject : {self.subject} Id: {self.id}'
+    
+class School :
+    def __init__(self, name ) -> None:
+        self.name = name
+        self.teachers = []
+        self.students = []
+    
+    def add_teacher(self, name, subject):
+        id = len(self.teachers) +101
+        teacher = Teacher(name,subject,id)
+        self.teachers.append(teacher)
+    def enrolled(self, name, fee):
+        
+        if (fee<4500):
+            print("Need more fee to Enrolled")
+        else:
+            id = len(self.students) + 101
+            student = Student(self.name,"C",id)
+            self.students.append(student)
+        
 
-alia = Student("alia Torkari ", 9,1)
-print(alia)
-ranbeer = Teacher('Douran beer', 'Algorithm', 101)
-print(ranbeer)
+# alia = Student("alia Torkari ", 9,1)
+# print(alia)
+# ranbeer = Teacher('Douran beer', 'Algorithm', 101)
+# print(ranbeer)
