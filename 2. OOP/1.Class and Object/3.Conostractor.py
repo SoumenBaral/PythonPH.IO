11644955fd24c18760027f87403f169bf098cb18
SoumenBaral <shumen.sb@gmail.com>
2023-11-06 14:44:38 +0600

    Set a constructor ;

--- 2. OOP/1.Class and Object/3.Conostractor.py
+++ 2. OOP/1.Class and Object/3.Conostractor.py
@@ -1,6 +1,11 @@
 class phone :
     Manufacture = "Chain"
 
+    def __init__(self,owner,brand,price):
+        self.owner = owner
+        self.brand = brand
+        self.price = price
+
     def sendMMS(self,number,mms):
         text = f'we got the number of CM that is {number} and send MMs :: {mms} '
         print(text)
