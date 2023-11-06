b4d1c5fafc37d6f76d28423072acaa997749ad3f
SoumenBaral <shumen.sb@gmail.com>
2023-11-06 14:54:06 +0600

    Constructor

--- 2. OOP/1.Class and Object/3.Conostractor.py
+++ 2. OOP/1.Class and Object/3.Conostractor.py
@@ -9,3 +9,10 @@ class phone :
     def sendMMS(self,number,mms):
         text = f'we got the number of CM that is {number} and send MMs :: {mms} '
         print(text)
+
+
+my_phone = phone("kala Chan Pakhi", "Oppo",8900)
+print(my_phone.owner,my_phone.brand,my_phone.price)
+
+her_phone = phone("shada chani","Iphone",120000)
+print(her_phone.owner,her_phone.brand,her_phone.price)
