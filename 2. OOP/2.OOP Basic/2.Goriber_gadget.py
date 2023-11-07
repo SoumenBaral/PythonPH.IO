be2377fc1e30a46dd48c775c3b4ee077fca7ae43
SoumenBaral <shumen.sb@gmail.com>
2023-11-07 13:22:50 +0600

    gorib"

---
+++ 2. OOP/2.OOP Basic/2.Goriber_gadget.py
@@ -0,0 +1,30 @@
+class laptop:
+    def __init__(self,brand,color, price ,memory) -> None:
+        self.brand = brand
+        self.color = color 
+        self.price = price
+        self.memory = memory
+
+    def run (self):
+        print(f'Running laptop: {self.brand}')
+
+    def coding(self):
+        print(f"learning and practicing the via my {self.color} {self.brand} laptop")
+
+class Phone:
+    def __init__(self,brand,color, price , dual_sim) -> None:
+        self.brand = brand
+        self.color = color 
+        self.price = price
+        self.dual_sim = dual_sim
+        
+
+    def run (self):
+        print(f'Running Phone: {self.brand}')
+
+    def phone_call(self,number ,text):
+        print(f"Number : {number}  and SMS : {text}")
+
+hp = Phone("HP","Silver",25000,500)
+hp.run()
+hp.phone_call(2322,"I Hate you")
