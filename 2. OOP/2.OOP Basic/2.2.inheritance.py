86fae36d38039dfda47a524f38b2bd5e6ddcdadb
SoumenBaral <shumen.sb@gmail.com>
2023-11-07 14:19:25 +0600

    inheritance

--- 2. OOP/2.OOP Basic/2.2.inheritance.py
+++ 2. OOP/2.OOP Basic/2.2.inheritance.py
@@ -1,45 +1,50 @@
 # Base Class , Parent Class , Common Attribute , functionality Class
 # Child class , derivate class ,uncommon attribute + Functionality class 
 class Gadget:
-    def __init__(self,brand,color, price ) -> None:
+    def __init__(self,brand,color, price ,origin ) -> None:
         self.brand = brand
         self.color = color 
         self.price = price
-        pass
-class laptop:
-    def __init__(self,memory) -> None:
-        
-        self.memory = memory
-
+        self.origin = origin
+     
+    
     def run (self):
         print(f'Running laptop: {self.brand}')
+
+
+class laptop:
+    def __init__(self,memory,ssd) -> None:
+        self.ssd = ssd
+        self.memory = memory
 
     def coding(self):
         print(f"learning and practicing the via my {self.color} {self.brand} laptop")
 
-class Phone:
-    def __init__(self,brand,color, price , dual_sim) -> None:
-        self.brand = brand
-        self.color = color 
-        self.price = price
+class Phone(Gadget):
+    def __init__(self ,brand,color,price,origin, dual_sim) -> None:
         self.dual_sim = dual_sim
         
-
-    def run (self):
-        print(f'Running Phone: {self.brand}')
+        super().__init__(brand,color,price,origin)
 
     def phone_call(self,number ,text):
         print(f"Number : {number}  and SMS : {text}")
 
+    def __repr__(self) -> str:
+        return f'phone: {self.brand} {self.price} {self.dual_sim}'
+
 class Camera:
-    def __init__(self, brand, price, color, pixel) -> None:
-        self.brand = brand
-        self.price = price
-        self.color = color
+    def __init__(self, pixel) -> None:
         self.pixel = pixel
 
-    def run(self):
-        pass
 
     def change_lens(self):
         pass
+
+
+# Inheritance 
+my_phone = Phone('iphone', 120000, 'silver', 'china', True)
+
+print(my_phone)
+# my_phone.phone_call()
+print(my_phone.brand)
+print(my_phone)
