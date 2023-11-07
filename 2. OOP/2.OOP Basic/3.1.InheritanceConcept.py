7bdcac570f8420f7176cbdb25b104f8d4e507a27
SoumenBaral <shumen.sb@gmail.com>
2023-11-08 01:57:44 +0600

    inheritance concept

---
+++ 2. OOP/2.OOP Basic/3.1.InheritanceConcept.py
@@ -0,0 +1,14 @@
+# Parent Class , Base class
+
+class BaseClass:
+    pass # It will store all the common element and function
+
+# Derived Class , Child class 
+class derivedClass(BaseClass):
+    pass
+"""
+1. Simple inheritance : parent --> Child 
+2. Multi-level inheritance : GrandPa-->parent-->child
+3. Multiple inheritance  : Student(family,school ,sports )
+4. Hybrid inheritance    : father , uncle ,Aunty ---> Child (father , uncle )
+"""
