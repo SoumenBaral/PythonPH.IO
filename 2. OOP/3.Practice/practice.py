d0c8918c3427f0522333f4eb637d89ddae6189c9
SoumenBaral <shumen.sb@gmail.com>
2023-11-08 13:41:23 +0600

    practice done

--- 2. OOP/3.Practice/practice.py
+++ 2. OOP/3.Practice/practice.py
@@ -4,3 +4,23 @@ class Product:
         self.products = []
     
 
+class Shop(Product):
+    def __init__(self, product) -> None:
+        super().__init__(product)
+
+
+    def add_product(self,product):
+        self.products.append(product)
+
+    def byProduct(self,product):
+       
+        if(product in self.products):
+            print("Thank you so much For visiting and buying some thing from our shop ")
+
+
+
+watch = Shop("Watch")
+watch.add_product("Watch")
+watch.add_product("Add nothing ")
+print(watch.products)
+watch.byProduct("Watch")
