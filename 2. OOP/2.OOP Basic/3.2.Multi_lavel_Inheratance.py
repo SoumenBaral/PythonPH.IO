8abf389632c01e63d7c91264b046c8f7721f17e6
SoumenBaral <shumen.sb@gmail.com>
2023-11-08 09:32:03 +0600

    muli level

--- 2. OOP/2.OOP Basic/3.2.Multi_lavel_Inheratance.py
+++ 2. OOP/2.OOP Basic/3.2.Multi_lavel_Inheratance.py
@@ -13,5 +13,28 @@ class Bus(Vehicle):
     def __repr__(self) -> str:
         return super().__repr__()
 
+class Truck(Vehicle):
+    def __init__(self, name, price,weight) -> None:
+        self.weight = weight
+        super().__init__(name, price)
+class Pickup(Truck):
+    def __init__(self, name, price, weight) -> None:
+        super().__init__(name, price, weight)
+
+class AcBus(Bus):
+    def __init__(self, name, price, seat,temperature) -> None:
+        self.temperature = temperature
+        super().__init__(name, price, seat)
+    
+    def __repr__(self) -> str:
+        print(self.temperature)
+        return super().__repr__()
+       
+
+
 EnaPoribohon = Bus("Ena PoriBohon ",1200,56)
 print(EnaPoribohon)
+
+
+green_line = AcBus('green', 5000000, 22, 16)
+print(green_line)
