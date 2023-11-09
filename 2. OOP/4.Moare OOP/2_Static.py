e3f866ff97438241f22b61fe133a5eee59c5c60a
SoumenBaral <shumen.sb@gmail.com>
2023-11-09 18:01:24 +0600

    class method

--- 2. OOP/4.Moare OOP/2_Static.py
+++ 2. OOP/4.Moare OOP/2_Static.py
@@ -4,3 +4,13 @@ class Shopping:
     def __init__(self,name ,location ) -> None:
         self.name = "Jamuna City" # Instance Attribute 
         self.location = "near to all janina"
+    def purchase(self,item ,price ,amount):
+        remaining = amount - price
+        print(f"Buying {item} for Price {price} and remaining {remaining}")
+    
+    @classmethod # when we just call the class and it's method for ignoring the self parameter we have to use @classmethod
+    def hudaiDakhi(self,item):
+        print("hudai daktachi but kisui kintam na Ac ar hawa khaibar aschii",item)
+
+# Shopping.purchase(3,3,5,6)
+Shopping.hudaiDakhi("lungi")
