c55d8645ecb73da088d9b4e7d24ea5212e3a5ee1
SoumenBaral <shumen.sb@gmail.com>
2023-11-09 18:17:58 +0600

    Static

--- 2. OOP/4.Moare OOP/2_Static.py
+++ 2. OOP/4.Moare OOP/2_Static.py
@@ -1,4 +1,11 @@
+# static attribute (class attribute)
+# static method @staticmethod
+# class method @classmethod
+# differences between static method and class method
+
+
 class Shopping:
+    
     cart = [] # class Attribute or Static Attribute 
     Origin = "japan"
     def __init__(self,name ,location ) -> None:
@@ -8,9 +15,21 @@ class Shopping:
         remaining = amount - price
         print(f"Buying {item} for Price {price} and remaining {remaining}")
     
-    @classmethod # when we just call the class and it's method for ignoring the self parameter we have to use @classmethod
+    @classmethod
+    # when we just call the class and it's method for ignoring the self parameter we have to use @classmethod
     def hudaiDakhi(self,item):
         print("hudai daktachi but kisui kintam na Ac ar hawa khaibar aschii",item)
 
+    @staticmethod
+    # When we use it we don't need any self keyword to derive it
+    def Multiply(a,b):
+        print(a*b)
+
+
+
+basundara = Shopping('basu en dhara', 'not popular location')
+# basundara.purchase('lungi', 500, 1000)
+basundara.hudaiDakhi('lungi')
 # Shopping.purchase(3,3,5,6)
 Shopping.hudaiDakhi("lungi")
+Shopping.Multiply(2,3)
