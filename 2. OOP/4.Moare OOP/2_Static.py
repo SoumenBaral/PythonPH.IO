6292de136223a65504341241983f1dff072676c1
SoumenBaral <shumen.sb@gmail.com>
2023-11-09 17:34:24 +0600

    Class Attribute and instance attribute

---
+++ 2. OOP/4.Moare OOP/2_Static.py
@@ -0,0 +1,6 @@
+class Shopping:
+    cart = [] # class Attribute or Static Attribute 
+    Origin = "japan"
+    def __init__(self,name ,location ) -> None:
+        self.name = "Jamuna City" # Instance Attribute 
+        self.location = "near to all janina"
