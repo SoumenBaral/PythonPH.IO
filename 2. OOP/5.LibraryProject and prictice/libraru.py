a6c51ece00d6022ab4f633f7de26e501c3e22ce4
SoumenBaral <shumen.sb@gmail.com>
2023-11-10 13:37:45 +0600

    add user and add book

---
+++ 2. OOP/5.LibraryProject and prictice/libraru.py
@@ -0,0 +1,45 @@
+class Book :
+    def __init__(self,name,id,author,quantity) -> None:
+        self.name     = name
+        self.id       = id
+        self.author   = author
+        self.quantity = quantity
+
+class User:
+    def __init__(self,name,id,password) -> None:
+        self.name        = name 
+        self.id          = id
+        self.password    = password
+        self.borrowBooks = []
+        self.returnBooks = []
+
+class Library:
+    def __init__(self,name) -> None:
+        self.name = name
+        self.users = []
+        self.books = []
+
+    def addBook(self,id,name,quantity,author):
+        for book in self.books:
+            if book.id == id:
+                print(f"\n\t---> !! Book Id {book.id} already exists")
+                return
+        book = Book(name,id,author,quantity)
+        self.books.append(book)
+        print(f"{book.quantity} of book : {book.name}  added in the library successfully")
+
+
+    def addUser(self,name,id,password):
+
+        for user in self.users:
+            if user.id == id:
+                print(f'{id} user is Already exist')
+                return
+        user = User(name,id,password)
+        self.users.append(user)
+        print(f" Welcome to our library MemberShip sir, {user.name}")
+        return user
+
+bsk = Library("Waste of time ")
+bsk.addBook(101,"cp_Boss",4,"Dane Danial")
+Soumen =bsk.addUser("Soumen",111,"func")       
