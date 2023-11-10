557cbee1293062b3d11bfa4f2c046fb1439f2de7
SoumenBaral <shumen.sb@gmail.com>
2023-11-10 08:22:42 +0600

    inharitance

---
+++ 2. OOP/4.Moare OOP/6_Inheritace.py
@@ -0,0 +1,26 @@
+# Inheritance provides you in a relation
+
+class  Animal:
+    pass
+
+# Dog is a animal
+class Dog(Animal):
+    pass
+
+# cat is a Animal
+class Cat(Animal):
+    pass
+
+class Furniture:
+    pass
+
+# table is a furniture
+
+class Table(Furniture):
+    pass
+
+# chair is furniture :
+
+class chair(Furniture):
+    pass
+
