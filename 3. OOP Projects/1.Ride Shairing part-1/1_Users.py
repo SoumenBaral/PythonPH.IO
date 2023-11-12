6ffe44fa29927becd3d3ee192b5a382f052e3c82
SoumenBaral <shumen.sb@gmail.com>
2023-11-12 10:03:24 +0600

    add 2 methon display profile and Request a ride '

--- 3. OOP Projects/1.Ride Shairing part-1/1_Users.py
+++ 3. OOP Projects/1.Ride Shairing part-1/1_Users.py
@@ -12,3 +12,19 @@ class Users(ABC):
     @abstractmethod
     def displayProfile(self):
         raise NotImplementedError
+
+class Rider(Users):
+    def __init__(self, name, email, NID) -> None:
+        self.CurrentRide = None
+        super().__init__(name, email, NID)
+
+    def displayProfile(self):
+        print(f"Rider Name : {self.name} and his Email: {self.email}")
+    
+    def requestRide(self,currentLocation,destination):
+        if not self.CurrentRide:
+
+            # TODO: set ride request property
+            # TODO: Set current via ride match
+            ride_request = None
+            self.CurrentRide = None
