913eba475e5acd9682ed5b2108cbd8be7585c25b
SoumenBaral <shumen.sb@gmail.com>
2023-11-13 22:07:14 +0600

    concept done

---
+++ 3. OOP Projects/2.Restaurant opp/concept.py
@@ -0,0 +1,32 @@
+"""
+    1.inheritance 
+    2. polymorphism 
+    3.abstraction 
+    4.encapsulation 
+-----------------------------------------------
+
+Class
+1.User : Base class  (ABC)
+2.Customer(User)
+3.Employee(User)
+4.chef(Employee)
+5.Server(Employee)
+6.Manager(Employee)
+7.Cleaner(Employee)
+8.Supplier(User)
+9.Marketer(User)
+
+
+
+Another class system :
+----------------------
+1.FoodItem(ABC) Base class
+2.Pizza (FoodItem)
+3.Burger(FoodItem)
+4.Drinks(FoodItem)
+5.juices (FoodItem)
+6.Salad(FoodItem)
+
+
+1.Order
+"""
