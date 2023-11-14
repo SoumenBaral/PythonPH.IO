447cf04dac8de072dd1eaf47025d52cac2368e70
SoumenBaral <shumen.sb@gmail.com>
2023-11-15 01:13:22 +0600

    add some new class' g

--- 3. OOP Projects/3.Practice and recap/Conceptual Session/Main.py
+++ 3. OOP Projects/3.Practice and recap/Conceptual Session/Main.py
@@ -17,6 +17,50 @@ class RideSharing :
         return f'{self.company_name} has  Driver :{len(self.drivers)} and Riders : {len(self.riders)}'
 
 
+class User(ABC):
+    def __init__(self,name , email,nid) -> None:
+        super().__init__()
+        self.name = name 
+        self.email = email
+        self.nid = nid
+
+    @abstractmethod
+    def display_Profile(self):
+        raise NotImplementedError
+
+class Driver(User):
+    def __init__(self, name, email, nid,currentLocation) -> None:
+        super().__init__(name, email, nid)
+        self.currentLocation = currentLocation
+    def display_Profile(self):
+        print(f"Driver name is {self.name} and mail is {self.email}")
+class Rider(User):
+    def __init__(self, name, email, nid ,currentLocation) -> None:
+        super().__init__(name, email, nid)
+        self.currentLocation = currentLocation
+        self.destination = destination
+        self.currentRide = None
+
+    def display_Profile(self):
+        print(f"Rider  name is {self.name} and mail is {self.email}")
+
+    def rideRequest(self,ridingApp,destination):
+        print("looking for Rides")
+        if not self.currentRide :
+            pass
+
+class Ride :
+    def __init__(self,startLocation,endLocation ) -> None:
+        self.startLocation = startLocation
+        self.endLocation = endLocation
+        self.driver = None
+        self.rider = None 
+    def start_ride(self):
+        pass
+    def endRide(self):
+        pass
+    def __repr__(self) -> str:
+        return f"Start from {self.start_location} to {self.endLocation}"         
 
 uber = RideSharing("UBER")
 uber.addDriver("monu")
