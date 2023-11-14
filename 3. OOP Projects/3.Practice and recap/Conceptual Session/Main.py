1809791dc5f4054d522aa5e3246ac293713d5ea3
SoumenBaral <shumen.sb@gmail.com>
2023-11-14 21:51:07 +0600

    create a Ride shairing class and do more '

---
+++ 3. OOP Projects/3.Practice and recap/Conceptual Session/Main.py
@@ -0,0 +1,23 @@
+from abc import ABC , abstractmethod
+# we use Abstractmethod for base class and take some must taking method in inherit class
+# __init or anything __ is called Dander method
+
+# First we need a company's blueprint that why we can use it any where
+class RideSharing :
+    def __init__(self,company_name) -> None: 
+        self.company_name = company_name
+        self.drivers = []
+        self.riders = []
+    def addDriver(self,driver):
+        self.drivers.append(driver)
+    def addRider(self,rider):
+        self.riders.append(rider)
+    
+    def __repr__(self) -> str:
+        return f'{self.company_name} has  Driver :{len(self.drivers)} and Riders : {len(self.riders)}'
+
+uber = RideSharing("UBER")
+uber.addDriver("monu")
+uber.addRider("janu")
+print(uber)
+        
