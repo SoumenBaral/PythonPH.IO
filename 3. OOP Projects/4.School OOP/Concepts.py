02584fd4e4f22cc7057bbb3d7bf0ee3fde5e90aa
SoumenBaral <shumen.sb@gmail.com>
2023-11-15 06:15:21 +0600

    high level thinking

---
+++ 3. OOP Projects/4.School OOP/Concepts.py
@@ -0,0 +1,21 @@
+"""
+--------------------------------------
+                HIGH LEVEL AREA
+---------------------------------------
+            School System Development 
+
+1.Student Management
+2.Teacher Management
+3.cafeteria
+4.Sports Management 
+5.Club Management 
+6.Event Management
+7.Library Management 
+8.Assessment
+
+----------------------------------
+        CORE FUNCTIONALITIES,
+
+
+
+"""
