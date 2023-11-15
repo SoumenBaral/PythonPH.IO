c7d2f2ad98bd04412f06c74ebcac887db9e4b983
SoumenBaral <shumen.sb@gmail.com>
2023-11-15 19:36:56 +0600

    bank add .

---
+++ 3. OOP Projects/5.Practice and CS/Conceptual session/Bank.py
@@ -0,0 +1,39 @@
+
+class Account:
+    accounts = []
+    def __init__(self,name,accountNumber,Password,type) -> None:
+        self.name = name 
+        self.accountNo = accountNumber
+        self.password = Password
+        self.type = type 
+        self.balance = 0
+        Account.accounts.append(self)
+    def changeInfo(self,name):
+        self.name = name
+        print(f"\n--> Name is changed of {self.accountNo}")
+
+    # Overloading of method change Info 
+
+    def changeInfo(self,name,passWord):
+        self.name = name 
+        self.password = passWord
+        print(f"\n--> Name is changed of {self.accountNo} -- and Name:{self.name} and PassWord :{self.password}")
+
+    def deposit(self,amount,):
+        if amount>0:
+            self.balance += amount
+            print(f"\n--> Deposited {amount}. New balance: ${self.balance}")
+
+        else :
+            print("\n Invalid Deposit \n ")
+    def Withdrew(self,amount,):
+        if amount>0 and self.balance>=amount:
+            self.balance -= amount
+            print(f"\n--> Withdrew  {amount}. New balance: ${self.balance}")
+
+        else :
+            print("\n Invalid Withdrew \n ")
+
+
+    
+
