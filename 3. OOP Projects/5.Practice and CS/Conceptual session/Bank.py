65c1ab42f0881019122eba752938d026c43badc0
SoumenBaral <shumen.sb@gmail.com>
2023-11-15 20:06:52 +0600

    all done

--- 3. OOP Projects/5.Practice and CS/Conceptual session/Bank.py
+++ 3. OOP Projects/5.Practice and CS/Conceptual session/Bank.py
@@ -23,9 +23,10 @@ class Account:
         if amount>0:
             self.balance += amount
             print(f"\n--> Deposited {amount}. New balance: ${self.balance}")
-
         else :
             print("\n Invalid Deposit \n ")
+
+
     def Withdrew(self,amount,):
         if amount>0 and self.balance>=amount:
             self.balance -= amount
@@ -44,25 +45,6 @@ class SavingsAccount(Account):
         super().__init__(name, accountNumber, Password, "SavingsAccount")
         self.interestRate = interestRate
     
-    def Withdrew(self, amount):
-        if amount > 0 and (self.balance - amount) >= -self.limit:
-            self.balance -= amount
-            print(f"\n--> Withdrew ${amount}. New balance: ${self.balance}")
-        else:
-            print("\n--> Invalid withdrawal amount or overdraft limit reached")
-        
-
-    def showInfo(self):
-        print(f"Infos of {self.type} account of {self.name}:\n")
-        print(f'\n\tAccount Type : {self.type}')
-        print(f'\tName : {self.name}')
-        print(f'\tAccount No : {self.accountNo}')
-        print(f'\tCurrent Balance : {self.balance}\n')
-
-class SpecialAccount(Account):
-    def __init__(self, name, accountNumber, Password,limit) -> None:
-        super().__init__(name, accountNumber, Password, "special")
-        self.limit = limit
     
     def applyInterest(self):
         Interest = self.balance*(self.interestRate/100)
@@ -76,3 +58,119 @@ class SpecialAccount(Account):
         print(f'\tName : {self.name}')
         print(f'\tAccount No : {self.accountNo}')
         print(f'\tCurrent Balance : {self.balance}\n')
+
+class SpecialAccount(Account):
+    def __init__(self, name, accountNumber, Password,limit) -> None:
+        super().__init__(name, accountNumber, Password, "special")
+        self.limit = limit
+    
+   
+    def Withdrew(self, amount):
+        if amount > 0 and (self.balance - amount) >= self.limit:
+            self.balance -= amount
+            print(f"\n--> Withdrew ${amount}. New balance: ${self.balance}")
+        else:
+            print("\n--> Invalid withdrawal amount or overdraft limit reached")
+        
+
+    def showInfo(self):
+        print(f"Infos of {self.type} account of {self.name}:\n")
+        print(f'\n\tAccount Type : {self.type}')
+        print(f'\tName : {self.name}')
+        print(f'\tAccount No : {self.accountNo}')
+        print(f'\tCurrent Balance : {self.balance}\n')
+
+
+
+currentUser=None
+
+while(True):
+    if currentUser==None:
+        print("\n--> No user logged in !")
+        ch=input("\n--> Register/Login (R/L) : ")
+        if ch=="R":
+            name=input("Name:")
+            no=input("Account Number:")
+            pa=input("Password:")
+            a=input("Savings Account or special Account (sv/sp) :")
+            if a=="sv":
+                ir=int(input("Interest rate:"))
+                currentUser=SavingsAccount(name,no,pa,ir)
+            else:
+                lm=int(input("Overdraft Limit:"))
+                currentUser=SpecialAccount(name,no,pa,lm)
+        else:
+            no=int(input("Account Number:"))
+            for account in Account.accounts:
+                
+                if account.accountNo==no:
+                    currentUser=account
+                    break
+                
+    else:
+        print(f"\nWelcome {currentUser.name} !\n")
+        
+        if currentUser.type=="savings":
+            
+            print("1. Withdraw")
+            print("2. Deposit")
+            print("3. Show Info")
+            print("4. change Info")
+            print("5. Apply Interset")
+            print("6. Logout\n")
+            
+            op=int(input("Chhose Option:"))
+            
+            if op==1:
+                amount=int(input("Enter withdraw amount:"))
+                currentUser.withdraw(amount)
+                
+            elif op==2:
+                amount=int(input("Enter deposit amount:"))
+                currentUser.deposit(amount)
+            
+            elif op==3:
+                currentUser.showInfo()
+            
+            elif op==4:
+                print("Homework")
+            
+            elif op==5:
+                currentUser.apply_interest()
+            
+            elif op==6:
+                currentUser=None
+            else:
+                print("Invalid Option")
+        
+        else:
+            print("1. Withdraw")
+            print("2. Deposit")
+            print("3. Show Info")
+            print("4. change Info")
+            print("5. Logout\n")
+            
+            
+            op=int(input("Chhose Option:"))
+            
+            if op==1:
+                amount=int(input("Enter withdraw amount:"))
+                currentUser.Withdrew(amount)
+                
+            elif op==2:
+                amount=int(input("Enter deposit amount:"))
+                currentUser.deposit(amount)
+            
+            elif op==3:
+                currentUser.showInfo()
+            
+            elif op==4:
+                print("Homework")
+            
+            elif op==5:
+                # currentUser=None
+                break # when we use break it will be out of the system 
+            
+            
+            else:
+                print("Invalid Option")
