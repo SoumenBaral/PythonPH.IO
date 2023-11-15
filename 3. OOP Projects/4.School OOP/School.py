53701a042180055e124b08e904a752c1aaca07a7
SoumenBaral <shumen.sb@gmail.com>
2023-11-15 12:46:56 +0600

    add some stataic method

--- 3. OOP Projects/4.School OOP/School.py
+++ 3. OOP Projects/4.School OOP/School.py
@@ -20,6 +20,53 @@ class School:
         else:
             print(f'No classroom as a {className}')
 
+    @staticmethod
+    def calculate_grade(marks):
+        if 80 <= marks <= 100:
+            return 'A+'
+        elif 70 <= marks < 80:
+            return 'A'
+        elif 60 <= marks < 70:
+            return 'A-'
+        elif 50 <= marks < 60:
+            return 'B'
+        elif 40 <= marks < 50:
+            return 'C'
+        elif 33 <= marks < 40:
+            return 'D'
+        else:
+            return 'F'
+        
+    @staticmethod
+    def grade_to_value(grade):
+        grade_map = {
+            'A+': 5.00, 
+            'A': 4.00, 
+            'A-': 3.50, 
+            'B': 3.00, 
+            'C': 2.00, 
+            'D': 1.00, 
+            'F': 0.00
+            }
+        return grade_map[grade]
+
+    @staticmethod
+    def value_to_grade(value):
+        if 4.5 <= value <= 5.00:
+            return 'A+'
+        elif 3.5 <= value < 4.5:
+            return 'A'
+        elif 3.0 <= value < 3.5:
+            return 'A-'
+        elif 2.5 <= value < 3.0:
+            return 'B'
+        elif 2.0 <= value < 2.5:
+            return 'C'
+        elif 1.0 <= value < 2.0:
+            return 'D'
+        else:
+            return 'F'
+
     def __repr__(self) -> str:
         print("---------------All Classrooms----------")
 
